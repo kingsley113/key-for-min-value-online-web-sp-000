823a86f8578adcefc92fe84710ecdd01fc1c10b0
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-18 08:03:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest_value = ""
+  name_hash.each do |name, value|
+    if lowest_value != ""
+      if value < lowest_value
+        lowest_value = value
+        lowest_name = name
+      end
+    else
+      lowest_value = value
+      lowest_name = name
+    end
 
+    lowest_name
 end
